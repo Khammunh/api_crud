d39ce2f4623110fdfebbce3f49b77ebe84cc30f0
Khammunh <khamlalago12345@gmail.com>
2024-05-06 10:39:02 +0700

    first api crud

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MyAPP(),
+  );
+}
+class MyAPP extends StatelessWidget {
+  const MyAPP({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ,
+    );
+  }
+}
