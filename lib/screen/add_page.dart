8017efbe19a578c2adeefd74836ff11714af5ddf
Khammunh Thao <100525451+Khammunh@users.noreply.github.com>
2024-05-07 21:15:42 +0700

    api GET

--- lib/screen/add_page.dart
+++ lib/screen/add_page.dart
@@ -1,8 +1,18 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class AddTodoPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AddTodoPage extends StatefulWidget {
   const AddTodoPage({super.key});
 
+  @override
+  State<AddTodoPage> createState() => _AddTodoPageState();
+}
+
+class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,31 +22,80 @@ class AddTodoPage extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(10),
         children: [
-          const TextField(
-            decoration: InputDecoration(hintText: 'title'),
+          TextField(
+            controller: titleController,
+            decoration: const InputDecoration(hintText: 'title'),
           ),
-          const TextField(
-            decoration: InputDecoration(hintText: 'Description'),
+          TextField(
+            controller: descriptionController,
+            decoration: const InputDecoration(hintText: 'Description'),
             maxLines: 8,
             minLines: 5,
             keyboardType: TextInputType.multiline,
           ),
-          SizedBox(height: 50.0),
+          const SizedBox(height: 50.0),
           ElevatedButton(
             style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-            onPressed: () {},
+            onPressed: submitData,
             child: const Text(
               'Submit',
-              style: TextStyle(
-                color: Colors.white,
-              ),
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
             ),
           )
         ],
       ),
     );
   }
-  void submitData() {
-    
+  
+
+
+  //API POST  
+  Future<void> submitData() async {
+    // Get the data from form
+    final title = titleController.text;
+    final description = descriptionController.text;
+
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false
+    };
+    // Submit data to the server
+    const url = 'https://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+    final response = await http.post(
+      uri,
+      body: jsonEncode(body),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    // show success or fail message based on status
+    if (response.statusCode == 201) {
+      titleController.text = '';
+      descriptionController.text = '';
+      print('Creation Success');
+      showSuccessMessage('Creation Success');
+    } else {
+      print('Creation Failed');
+      print(response.body);
+      showErrorMessage('Creation Failed');
+    }
+  }
+
+  void showSuccessMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      backgroundColor: Colors.blue,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
--- lib/screen/todo_list.dart
+++ lib/screen/todo_list.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:crud/screen/add_page.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class TodoListPage extends StatefulWidget {
   const TodoListPage({super.key});
@@ -9,6 +12,13 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  List items = [];
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,13 +28,6 @@ class _TodoListPageState extends State<TodoListPage> {
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: navigateToAddPage,
-        // child: IconButton(
-        //   onPressed: () {},
-        //   icon: const Icon(
-        //     Icons.add,
-        //     size: 30,
-        //   ),
-        // ),
         label: const Text('Add Todo'),
       ),
     );
@@ -35,5 +38,22 @@ class _TodoListPageState extends State<TodoListPage> {
       builder: (context) => const AddTodoPage(),
     );
     Navigator.push(context, route);
+  }
+
+  Future<void> fetchTodo() async {
+    const url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final result = json['item'] as List;
+      setState(() {
+        items = result;
+      });
+    } else {
+      print(response.body);
+      // learn here
+    }
   }
 }
